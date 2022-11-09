a39edc72932b3ad2fadb595e62f23c060a37cbb4
Pushp Vashisht <pushptyagi1@gmail.com>
2022-11-09 23:15:41 +0000

    commented rack app

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("rack/", include("rack.urls")),
+    # path("rack/", include("rack.urls")),
     path("admin/", admin.site.urls),
 ]
