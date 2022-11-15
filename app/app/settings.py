7ec5ffc7a565912290e5b857f52f561bcd850737
Pushp Vashisht <pushptyagi1@gmail.com>
2022-11-15 03:45:02 +0000

    Added Kiosk app

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "kiosk.apps.KioskConfig",
     "rack.apps.RackConfig",
     "django.contrib.admin",
     "django.contrib.auth",
--- app/app/urls.py
+++ app/app/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("kiosk/", include("kiosk.urls")),
     path("rack/", include("rack.urls")),
     path("admin/", admin.site.urls),
 ]
