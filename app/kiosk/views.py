14722d8d31eebf64b970455f922498447c7ff5d8
yashdhurde <122102731@umail.ucc.ie>
2022-11-17 21:54:22 +0000

    Added the user-deposit-payment.html and updated the relevant scripts

--- app/kiosk/views.py
+++ app/kiosk/views.py
@@ -20,13 +20,31 @@ def user_registration(request):
         phone = request.POST.get("phone", "")
         password = request.POST.get("pin", "")
         password_confirmation = request.POST.get("confirm-pin", "")
-        return HttpResponseRedirect("/kiosk/user/register/success/")
+        return HttpResponseRedirect("/kiosk/user/register/deposit-payment/")
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = UserForm()
 
     return render(request, "kiosk/user-registration.html", {"form": form})
+
+
+# view for battery deposit payment based on the deposit count
+def user_deposit_payment(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        deposit_amount = request.POST.get("deposit_amount", "")
+        card_number = request.POST.get("card_number", "")
+        name_on_card = request.POST.get("name_on_card", "")
+        cvv = request.POST.get("cvv", "")
+        expiry = request.POST.get("expiry", "")
+        return HttpResponseRedirect("/kiosk/user/register/success/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = UserForm()
+
+    return render(request, "kiosk/user-deposit-payment.html", {"form": form})
 
 
 def user_registration_success(request):
