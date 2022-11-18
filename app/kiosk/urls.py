234a83e78880b4dc9392c8de99cf0144fb9aeed1
Pushp Vashisht <pushptyagi1@gmail.com>
2022-11-18 01:57:46 +0000

    User login using qrcode scan completed

--- app/kiosk/urls.py
+++ app/kiosk/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("user/login/", views.user_login, name="user_login"),
+    path("user/decode_camera_feed/", views.decode_camera_feed, name="decode_camera_feed"),
     path("user/register/", views.user_registration, name="user_registration"),
     path(
         "user/register/deposit-payment",
@@ -13,4 +15,5 @@ urlpatterns = [
     path(
         "user/register/success/", views.user_registration_success, name="user_registration_success"
     ),
+    path("user/login/auth/<str:user_id>/", views.qr_scan_success, name="user_login_auth"),
 ]
