b8ef387fa569b519c94e1c86a80845d7b7cb1e96
yashdhurde <122102731@umail.ucc.ie>
2022-11-20 13:33:44 +0000

    Altered the battery_deposit_count and pin field in models.py

--- app/kiosk/models.py
+++ app/kiosk/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # create the Kiosk_user table
 class Users(models.Model):
     user_id = models.UUIDField(primary_key=True)
-    pin = models.IntegerField(max_length=4, null=False)
+    pin = models.IntegerField(null=False)
     name = models.CharField(max_length=45, null=False)
     email_id = models.CharField(max_length=45, unique=True, null=False)
     phone_no = models.CharField(max_length=15, null=False)
     driving_license = models.CharField(max_length=45, null=False, unique=True)
-    battery_deposit_count = models.IntegerField(max_length=5, null=True)
+    battery_deposit_count = models.IntegerField(default=0, null=True)
     admin_flag = models.BooleanField(null=False, default="False")
